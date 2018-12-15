a9f222f7886eeb608be53bd989a7fbb02051a765
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2018-12-14 19:42:49 -0600

    Create 141. Linked List Cycle.java

---
+++ src/141. Linked List Cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+ //使用快慢指针，如果有环（环是最后一个Node和前面的某个Node相连形成），最后两个指针一定相等
+ 
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
