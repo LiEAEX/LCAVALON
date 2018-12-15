1b33fa4c67a7ebfd29ad9f6c621b52bb0447540a
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2018-12-15 14:40:57 -0600

    Update 206. Reverse Linked List.java

--- src/206. Reverse Linked List.java
+++ src/206. Reverse Linked List.java
@@ -16,7 +16,7 @@ class Solution {
             ListNode tmp = cur.next; // 让tmp指向没有reverse过的最新的一个节点
             cur.next = tmp.next;     // 让cur指向节点与该新节点后面一个新节点相连（用于下一步更新tmp）
             tmp.next = dummy.next;   // 让tmp下一个点指向之前的头节点（即最新加入的节点的下一个点与之前reverse完的list相连，
-			                        // 完成到该新节点为止的reverse操作）
+			             // 完成到该新节点为止的reverse操作）
             dummy.next = tmp;        // 让dummy.next指向新的头节点（即最新的节点成为新的头节点）
         }
         return dummy.next;   
