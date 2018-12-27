d0e565be35efc6927763dc4079c724cf33c0ae0a
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2018-12-26 21:21:45 -0600

    70 561

---
+++ src/70. Climbing Stairs.java
@@ -0,0 +1,13 @@
+//DP bottom up
+class Solution {
+    public int climbStairs(int n) {
+        int[] stair = new int[n];
+        if (n < 2) return 1;
+        stair[0] = 1;
+        stair[1] = 2;
+        for (int i = 2; i < n; i++){
+            stair[i] = stair[i - 1] + stair[i - 2];
+        }
+        return stair[n - 1];
+    }    
+}
