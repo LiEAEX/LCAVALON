61f70ae47369a0e5a355a10d99f1aeaa01a3cd78
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2019-02-20 17:31:48 -0600

    Add files via upload

--- src/268. Missing Number.java
+++ src/268. Missing Number.java
@@ -7,3 +7,33 @@ class Solution {
         return (nums.length * (nums.length + 1) )/ 2 - sum;
     }
 }
+
+Binary Search:
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length, mid= (left + right)/2;
+        while(left<right){
+            mid = (left + right)/2;
+            if(nums[mid]>mid) right = mid;
+            else left = mid+1;
+        }
+        return left;
+    }
+}
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = -1;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++){
+            if (i != nums[i]){
+                res = i;
+                break;
+            }
+        }
+        if (res == -1) res = nums.length;
+        return res;        
+    }
+}
