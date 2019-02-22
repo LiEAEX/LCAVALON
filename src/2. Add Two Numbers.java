dae7f0bd73a4fe164c545e6c2e04206623615b4f
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2019-02-21 23:27:48 -0600

    Update 2. Add Two Numbers.java

--- src/2. Add Two Numbers.java
+++ src/2. Add Two Numbers.java
@@ -6,6 +6,43 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+Runtime: 19 ms, faster than 99.90% of Java online submissions for Add Two Numbers.
+Memory Usage: 48.3 MB, less than 14.25% of Java online submissions for Add Two Numbers.
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        int c = 0;
+        while (l1 != null && l2 != null){
+            dummy.next = new ListNode((l1.val + l2.val + c)%10);
+            c = l1.val + l2.val + c >= 10? 1: 0;
+            l1 = l1.next;
+            l2 = l2.next;
+            dummy = dummy.next;
+        }
+        while (l1 == null && l2 != null){
+            dummy.next = new ListNode((l2.val + c)%10);
+            c = c + l2.val >= 10? 1: 0;
+            l2 = l2.next;
+            dummy = dummy.next;
+        }
+        while (l2 == null && l1 != null){
+            dummy.next = new ListNode((l1.val + c)%10);
+            c = c + l1.val >= 10? 1: 0;
+            l1 = l1.next;
+            dummy = dummy.next;
+        }
+        if (l1 == null && l2 == null && c == 1){
+            dummy.next = new ListNode(1);
+            return cur.next;
+        }
+        return cur.next;
+    }
+}
+
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode d = new ListNode(666);
