9f451b8a60469d486e68d9fcd325a6a0a83e8c32
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2019-02-24 17:13:44 -0600

    Update 215. Kth Largest Element in an Array.java

--- src/215. Kth Largest Element in an Array.java
+++ src/215. Kth Largest Element in an Array.java
@@ -4,3 +4,37 @@ class Solution {
         return nums[nums.length - k];
     }
 }
+
+// Quickselect O(n), worstcase O(n^2)
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int start = 0, end = nums.length - 1, index = nums.length - k;
+        while (start < end) {
+            int pivot = partion(nums, start, end);
+            if (pivot < index) start = pivot + 1; 
+            else if (pivot > index) end = pivot - 1;
+            else return nums[pivot];
+        }
+        return nums[start];
+    }
+    
+    private int partion(int[] nums, int start, int end) {
+        int cur = nums[end];
+        int i = start - 1;
+        for (int j = start; j < end; j++){
+            if (nums[j] <= cur){
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, end);
+        return i + 1;
+    }
+    
+    private void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
