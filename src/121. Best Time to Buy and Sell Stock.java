2653f0a0abe188939dfd2717d44abd8658ee9d7a
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2019-02-26 09:52:32 -0600

    Update 121. Best Time to Buy and Sell Stock.java

--- src/121. Best Time to Buy and Sell Stock.java
+++ src/121. Best Time to Buy and Sell Stock.java
@@ -15,3 +15,21 @@ class Solution {
 	    return  max ;
 	 }
 }
+
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 35.7 MB, less than 93.98% of Java online submissions for Best Time to Buy and Sell Stock.
+	
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[] min = new int[prices.length];
+        int tp = Integer.MAX_VALUE, res = 0;
+        for (int i = 0; i < prices.length; i++){
+            min[i] = Math.min(tp, prices[i]);
+            tp = min[i];
+        }
+        for (int i = 0; i < prices.length; i++){
+            res = Math.max(res, prices[i] - min[i]);
+        }
+        return res;
+    }
+}
