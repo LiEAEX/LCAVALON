c4363c77b2d645fc1768eabad2e6e285f7c47b64
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2019-02-28 22:18:14 -0600

    Update 42. Trapping Rain Water_DP.java

--- src/42. Trapping Rain Water_DP.java
+++ src/42. Trapping Rain Water_DP.java
@@ -20,6 +20,29 @@ class Solution {
         return total;
     }
 }
+
+Key idea is to keep a track of the highest walls on the left and the right and narrowing down from both sides. 
+As we narrow down keeping adding to the result, the water trapped in unit width and smaller of each walls on each move.
 /*
 这种方法是基于动态规划Dynamic Programming的，我们维护一个一维的dp数组，这个DP算法需要遍历两遍数组，第一遍遍历dp[i]中存入i位置左边的最大值，然后开始第二遍遍历数组，第二次遍历时找右边最大值，然后和左边最大值比较取其中的较小值，然后跟当前值A[i]相比，如果大于当前值，则将差值存入结果
 */
+    
+class Solution {
+    public int trap(int[] height) {
+        int a = 0, b = height.length-1;
+        int max = 0, leftmax = 0, rightmax = 0;
+        while(a <= b){
+            leftmax = Math.max(leftmax, height[a]);
+            rightmax = Math.max(rightmax, height[b]);
+            if(leftmax < rightmax){
+                max += (leftmax - height[a]);       // leftmax is smaller than rightmax, so the (leftmax-A[a]) water can be stored
+                a++;
+            }
+            else{
+                max += (rightmax - height[b]);
+                b--;
+            }
+        }
+        return max; 
+    }
+} 
