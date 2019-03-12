65174ce3a26f28220f8f01cb49c8d3fcd688d570
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2019-03-11 19:28:14 -0500

    Add files via upload

---
+++ src/445. Add Two Numbers II.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        int sum = 0;
+        ListNode list = new ListNode(-1);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10; // 个位
+            ListNode head = new ListNode(sum / 10); // 进位
+            head.next = list; // 十位->个位
+            list = head; // 让list指向进位（对于十位和个位就是指向十位）
+            sum /= 10; // 取余
+        }
+        
+        return list.val == 0 ? list.next : list;
+    }
+}
