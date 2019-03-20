c656a72df19bc3be938bfc9a4243e3fd6282ecf5
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2019-03-20 10:09:45 -0500

    Update 206. Reverse Linked List.java

--- src/206. Reverse Linked List.java
+++ src/206. Reverse Linked List.java
@@ -6,9 +6,25 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null) return head; // iterative
+        //iterative
+        ListNode prev = null; // 第一个数之前的prev当然是空了
+        while(head != null){
+            ListNode next = head.next; // next为没有被reverse过的数的下一个点
+            head.next = prev; // 让新的head指向前一个数
+            prev = head; // 更新前一个数为现在的head
+            head = next; // 让现在的head移动到新的listnode上
+        }
+        return prev;
+    }
+}
+
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        /*if (head == null) return head; // iterative
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
         ListNode cur = head;
@@ -19,8 +35,9 @@ class Solution {
 			             // 完成到该新节点为止的reverse操作）
             dummy.next = tmp;        // 让dummy.next指向新的头节点（即最新的节点成为新的头节点）
         }
-        return dummy.next;
-	/*if (head == null || head.next == null) return head; // recursive
+        return dummy.next;*/
+	    
+	if (head == null || head.next == null) return head; // recursive
         ListNode p = head;
         head = reverseList(p.next);
         p.next.next = p;
@@ -28,7 +45,6 @@ class Solution {
         return head;*/
 	/*比如1->2->3这个最简单的链表，第一层时p指向1，head指向对2调用递归的结果，在第二层中，p指向2，
 	head指向对3调用递归的结果，此时递归返回3，所以第二层的head指向3，那么p.next.next=p的操作就把2连到了3的后面
-	，p.next=null这一步是把2后面断开，因为刚开始2后面连的是3。
-	*/ 
+	，p.next=null这一步是把2后面断开，因为刚开始2后面连的是3。	 
     }
 }
