b0139d0efc1f239b2c2d13493ad719e076369838
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2020-04-22 16:52:26 -0500

    Update 34. Find First and Last Position of Element in Sorted Array.java
    update

--- src/34. Find First and Last Position of Element in Sorted Array.java
+++ src/34. Find First and Last Position of Element in Sorted Array.java
@@ -1,5 +1,29 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) return new int[]{-1, -1};
+        int first = findTarget(nums, target);
+        int last = findTarget(nums, target + 1) - 1;
+        if (first == nums.length || nums[first] != target) return new int[]{-1, -1};
+        return new int[]{first, Math.max(first, last)};
+        
+    }
+    
+    public int findTarget(int[] nums, int target){
+        int l = 0, h = nums.length;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] >= target){
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return h;
+    }
+}
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right){
             int mid = left + (right - left) / 2 ;
@@ -20,3 +44,27 @@ class Solution {
         return res;
     }
 }  //O(logn)
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int idx = search(nums, 0, nums.length - 1, target);
+        if (idx == -1) {
+            int[] res = {-1, -1};
+            return res;
+        }
+        int left = idx, right = idx;
+        while (left > 0 && nums[left - 1] == nums[idx]) --left;
+        while (right < nums.length - 1 && nums[right + 1] == nums[idx]) ++right;
+        int[] res = {left, right};
+        return res; 
+    }
+    
+    int search(int[] nums, int left, int right, int target) {
+        if (left > right) return -1;
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) return search(nums, mid + 1, right, target);
+        else return search(nums, left, mid - 1, target);
+    }
+}
+// O(logn), worst case O(n)
