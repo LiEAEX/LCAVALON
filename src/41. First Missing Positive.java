0f1706798f7e5da80b67f5e017f24d6b84edc845
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2020-04-28 22:38:14 -0500

    Update 41. First Missing Positive.java

--- src/41. First Missing Positive.java
+++ src/41. First Missing Positive.java
@@ -1,3 +1,25 @@
+O(n), O(1)
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int tmp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[tmp - 1] = tmp;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
+    }
+}
+
+
+
+
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int res = 0;
