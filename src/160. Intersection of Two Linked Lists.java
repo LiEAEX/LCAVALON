611e68fc787726ecf61ca1d434515835078e02fa
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2020-05-01 16:10:36 -0500

    Update 160. Intersection of Two Linked Lists.java

--- src/160. Intersection of Two Linked Lists.java
+++ src/160. Intersection of Two Linked Lists.java
@@ -46,3 +46,50 @@ public class Solution {
         return len;
     }
 }
+
+
+
+Runtime: 1 ms, faster than 97.52% of Java online submissions for Intersection of Two Linked Lists.
+Memory Usage: 42.5 MB, less than 5.71% of Java online submissions for Intersection of Two Linked Lists.
+    
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        
+        ListNode cur1 = headA;
+        ListNode cur2 = headB;
+        
+        int len1 = 0, len2 = 0;
+        
+        while (cur1 != null){
+            cur1 = cur1.next;
+            len1++;
+        }
+        
+        while (cur2 != null){
+            cur2 = cur2.next;
+            len2++;
+        }
+        
+        cur1 = headA;
+        cur2 = headB;
+        
+        while (len1 > len2){
+            cur1 = cur1.next;
+            len1--;
+        }
+        
+        while (len2 > len1){
+            cur2 = cur2.next;
+            len2--;
+        }
+        
+        while (cur1 != null && cur2 != null){
+            if (cur1.val == cur2.val && cur1 == cur2) return cur1; // must check if cur1 and cur2 point to the same address
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        
+        return null;
+    }
+}
