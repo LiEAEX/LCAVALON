fa00969be82699e90f52167b79a92be39971a194
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2020-05-08 11:35:27 -0500

    Update 541. Reverse String II.java

--- src/541. Reverse String II.java
+++ src/541. Reverse String II.java
@@ -1,5 +1,30 @@
 class Solution {
     public String reverseStr(String s, int k) {
+        if (s == null || s.length() <= 1) return s;
+        char[] arr = s.toCharArray();
+        int len = s.length();
+        for (int i = 0; i < len; i += 2 * k){
+            int temp = Math.min(len - 1, i + k - 1);
+            reverse(arr, i, temp);
+        }
+        
+        return String.valueOf(arr);
+    }
+    
+    public void reverse(char[] arr, int a, int b){
+        while (a < b){
+            char tp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = tp;
+            a++;
+            b--;
+        }
+    }
+}
+
+
+class Solution {
+    public String reverseStr(String s, int k) {
         if (s.length() <= 1) return s;
         
         int len = s.length();
