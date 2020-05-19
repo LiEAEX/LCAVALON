fdc4b1b5aab8b41d114241a55e1039a26dbb0a85
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2020-05-19 16:00:34 -0500

    Update 744. Find Smallest Letter Greater Than Target.java

--- src/744. Find Smallest Letter Greater Than Target.java
+++ src/744. Find Smallest Letter Greater Than Target.java
@@ -16,7 +16,8 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         int l = 0, h = letters.length - 1;
         while (l <= h){
-            int m = l + (h - l) / 2;
+            int m = l + (h - l) / 2;  // if h and l are all big integers, l+h will cause integer overflow. 
+                                      // so use l + (h - l) / 2 instead of (h + l) / 2
             if (letters[m] <= target){
                 l = m + 1;
             }
