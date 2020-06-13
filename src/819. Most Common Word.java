318e88d156c277afbc136852b3587b24f741c63f
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2020-06-13 15:50:29 -0500

    Update 819. Most Common Word.java

--- src/819. Most Common Word.java
+++ src/819. Most Common Word.java
@@ -1,9 +1,9 @@
-Runtime: 16 ms, faster than 69.11% of Java online submissions for Most Common Word.
-Memory Usage: 37.6 MB, less than 59.97% of Java online submissions for Most Common Word.
+Runtime: 13 ms, faster than 47.52% of Java online submissions for Most Common Word.
+Memory Usage: 39.9 MB, less than 33.76% of Java online submissions for Most Common Word.
 
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        String[] words = paragraph.toLowerCase().split("[ !?',;.]+");
+        String[] words = paragraph.toLowerCase().split("\\W+"); // \\W means matches the nonword characters. It should be double slash.
         HashMap<String, Integer> map = new HashMap<>();
         for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
         for(String word : banned) if(map.containsKey(word)) map.remove(word);
