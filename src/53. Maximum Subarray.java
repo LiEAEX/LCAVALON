48bb84549d7aa8648f7232d43687753b03e68151
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2020-12-27 21:24:13 +0800

    Update 53. Maximum Subarray.java

--- src/53. Maximum Subarray.java
+++ src/53. Maximum Subarray.java
@@ -24,3 +24,16 @@ class Solution {                              //Divide and Conquer
 }
 //把数组一分为二，分别找出左边和右边的最大子数组之和，
 //然后从中间开始向左右分别扫描，求出最大值分别和左右两边得出的最大值相比较取最大
+    /*
+    Divide-and-conquer method.
+    The maximum summation of subarray can only exists under following conditions:
+    1. the maximum summation of subarray exists in left half.
+    2. the maximum summation of subarray exists in right half.
+    3. the maximum summation of subarray exists crossing the midpoints to left and right. 
+    1 and 2 can be reached by using recursive calls to left half and right half of the subarraies. 
+    Condition 3 can be found starting from the middle point to the left,
+    then starting from the middle point to the right. Then adds up these two parts and return. 
+    
+    T(n) = 2*T(n/2) + O(n)
+    this program runs in O(nlogn) time
+    */
