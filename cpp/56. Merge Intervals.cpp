b36231e5d987606ed3897d0541b9ae46aa9e2a1c
Avalon <avalonea6@163.com>
2022-12-11 19:19:10 +0800

    test

--- cpp/56. Merge Intervals.cpp
+++ cpp/56. Merge Intervals.cpp
@@ -1,6 +1,7 @@
 // 如1-6 2-4 3-8 -> 1-4 2-6 3-8 merge后的区间与把开始阶段，结束阶段的时间都sort，merge结果是不变的   -> 某种可证明的特性
 // 如果start的i+1大于end的i，则说明前面的区间是独立的，和后面不重叠
 
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
